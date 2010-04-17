a8cf30dcb1bc2b368a2d16e01383789501b2c1fb
Chris Pitzer <chris@lofiart.com>
2010-04-17 13:51:42 -0700

    basic html framework is in place.

--- html/js/code.js
+++ html/js/code.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
+var latlng = new google.maps.LatLng(-34.397, 150.644);
+var myOptions = {
+    zoom: 8,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 
 function correctHeight() {
     var window_height = $(window).height();
     var header_height = $("#search-container").offset().top;
     $("#search-results").height(window_height - header_height - 20); //-20 for padding
-    $("#search-map").height(window_height - header_height);
+    $("#map_canvas").height(window_height - header_height);
 }
 correctHeight();
 jQuery.event.add(window, "resize", correctHeight);
+
 });
